6891033d6857315ffcc668e4d4aded457cd27551
Gabriel Bueno <67878603+gbuenodev@users.noreply.github.com>
2022-01-28 16:44:54 -0300

    feat: cria pasta pública de images (#320)

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { router } = require('../routes/router');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/products', express.static(path.resolve(__dirname, '../../public/images')))
 
 app.use(router);
 
